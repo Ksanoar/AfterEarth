212b2fbf73c09c10ccf63abf1926c1ece3e62aaf
Ksanoar <ksanoar@gmail.com>
2013-07-22 11:56:42 +0200

    CORE

---
+++ GUI/Button.cpp
@@ -0,0 +1,19 @@
+#include "Button.h"
+#include "../Physic.h"
+
+void Button::draw() {
+	if(AABB::check(Object2D(_x, _y, _w, _h), Object2D((float)sf::Mouse::getPosition().x, (float)sf::Mouse::getPosition().y, 1, 1)))
+		mouseon = true;
+	else
+		mouseon = false;
+
+	if(mouseon == true && sf::Mouse::isButtonPressed(sf::Mouse::Left))
+		clicked = true;
+
+	_spr.setPosition(_x, _y);
+
+	if(mouseon == true) _spr.setTexture(_tex_hi);
+	else _spr.setTexture(_tex);
+
+	Window->draw(_spr);
+}
--- GUI/Button.h
+++ GUI/Button.h
@@ -4,5 +4,25 @@
 
 class Button {
 private:
+	float _x, _y;
+	float _w, _h;
+
+	sf::Texture _tex;
+	sf::Texture _tex_hi;
+	sf::Sprite  _spr;
 public:
+	bool mouseon;
+	bool clicked;
+public:
+	Button(float x, float y, float w, float h, std::string fname, std::string fname_hi) : _x(x), _y(y), _w(w), _h(h) { 
+		_tex.loadFromFile(fname);
+		_tex_hi.loadFromFile(fname_hi);
+		_spr.setTexture(_tex);
+
+		mouseon = false;
+		clicked = false;
+	}
+	~Button() { }
+
+	void draw();
 };
